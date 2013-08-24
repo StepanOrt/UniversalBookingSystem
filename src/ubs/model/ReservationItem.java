7168ff263df27b87187609f6ca895b6696e79433
Štěpán Ort <stepanort@gmail.com>
2013-08-24 15:33:19 +0200

    Tagy rezervačních položek

--- src/ubs/model/ReservationItem.java
+++ src/ubs/model/ReservationItem.java
@@ -6,19 +6,33 @@ import java.util.Set;
 public class ReservationItem {
 	private Set<ReservationAttributeValue> attributeValues;
 	private ReservationCategoryNode category;
+	private Set<ReservationTag> tags;
 	
 	public ReservationItem(ReservationCategoryNode category) {
 		attributeValues = new HashSet<ReservationAttributeValue>();
+		tags = new HashSet<ReservationTag>();
 		this.category = category;
 	}
 
-	public Set<ReservationAttributeValue> getAttributeValues() {
+	public Iterable<ReservationAttributeValue> getAttributeValues() {
 		return attributeValues;
 	}
 
 	public void addAttributeValue(ReservationAttribute attribute, Object value) {
 		ReservationAttributeValue attributeValue = new ReservationAttributeValue(attribute, value);
 		attributeValues.add(attributeValue);
+	}
+	
+	public void addTag(ReservationTag tag) {
+		tags.add(tag);
+	}
+	
+	public void removeTag(ReservationTag tag) {
+		tags.remove(tag);
+	}
+
+	public Iterable<ReservationTag> getTags() {
+		return tags;
 	}
 
 	public ReservationCategoryNode getCategory() {
